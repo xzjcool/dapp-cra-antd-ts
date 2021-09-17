613af417546c0c2d0143707261d3dd96d059cf34
zj <xzjcool@live.com>
2021-09-18 00:29:28 +0800

    feat: add i18n

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,13 @@
+import { Button } from 'antd'
 import React from 'react'
 import { useIntl } from 'react-intl'
 
 export const Home = () => {
   const { formatMessage, locale } = useIntl()
   console.log(locale)
-  return <div>{formatMessage({ id: 'Home' })} Page</div>
+  return (
+    <div>
+      <Button type="primary">{formatMessage({ id: 'Home' })} Page</Button>
+    </div>
+  )
 }
